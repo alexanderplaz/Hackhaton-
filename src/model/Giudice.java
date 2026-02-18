7291504fb8406a98800b3d59ed165e12b4be075d
alexanderplaz <75165344+alexanderplaz@users.noreply.github.com>
2026-02-18 22:40:09 +0100

    Delete src directory

--- src/model/Giudice.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Giudice extends Utente {
-    public Giudice(int id, String nome, String email) {
-        super(id, nome, email);
-    }
-}
--- src/model/Organizzatore.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Organizzatore extends Utente {
-    public Organizzatore(int id, String nome, String email) {
-        super(id, nome, email);
-    }
-}
